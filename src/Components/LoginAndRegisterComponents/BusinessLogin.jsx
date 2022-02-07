4b00eca1110933180bc1ccd9c41402581934bdba
Saurabh Sen <sensaurabh1212@gmail.com>
2022-02-07 09:17:00 +0530

    responsive design minor changes

--- src/Components/LoginAndRegisterComponents/BusinessLogin.jsx
+++ src/Components/LoginAndRegisterComponents/BusinessLogin.jsx
@@ -132,10 +132,9 @@ function BusinessLogin() {
   };
 
   return (
-    <div className="businessLogin">
+    <div className="businessLogin" style={{height: "100vh", }} >
       <section
-        className="vh-auto"
-        style={{ backgroundColor: "black", height: "100%" }}
+        style={{ backgroundColor: "black",  }}
       >
         <div className="container py-5 h-100">
           <div className="row d-flex align-items-center justify-content-center h-100">
